49d955f49a4a6157a4e7b6df0e6d3dbd4ce3b462
Jan Bróździak <jan.brozdziak@gmail.com>
2024-01-25 20:01:44 +0100

    Migration and uptate run

--- BrozdziakJankowski.BeerCatalog.DAO/Migrations/20240125190037_UpdateBeerTypeToEnum.Designer.cs
+++ BrozdziakJankowski.BeerCatalog.DAO/Migrations/20240125190037_UpdateBeerTypeToEnum.Designer.cs
@@ -2,6 +2,7 @@
 using BrozdziakJankowski.BeerCatalog.DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BrozdziakJankowski.BeerCatalog.DAO.Migrations
 {
     [DbContext(typeof(BeerCatalogContext))]
-    partial class BeerCatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20240125190037_UpdateBeerTypeToEnum")]
+    partial class UpdateBeerTypeToEnum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
---
+++ BrozdziakJankowski.BeerCatalog.DAO/Migrations/20240125190037_UpdateBeerTypeToEnum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrozdziakJankowski.BeerCatalog.DAO.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBeerTypeToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Beers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Beers",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
